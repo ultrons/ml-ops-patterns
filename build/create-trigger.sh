705a2bed475a09a4cd65549f206710e7542fc77d
Vaibhav Singh <sivaibhav@google.com>
2021-05-31 06:43:56 +0000

    initial draft

---
+++ build/create-trigger.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+# Continuous integration: recreate image anytime any file
+# in the directory that this script is run from is commited into GitHub
+# Run this only once per directory
+# In order to try this out, fork this repo into your personal GitHub account
+# Then, change the repo-owner to be your GitHub id
+
+REPO_NAME=ml-ops-patterns
+REPO_OWNER=sivaibhav
+
+#for trigger_name in trigger-000 trigger-001 trigger-002 trigger-003; do
+#  gcloud beta builds triggers delete --quiet $trigger_name
+#done
+
+
+gcloud beta builds triggers create github \
+  --build-config="./build/cloudbuild.yaml" \
+  --included-files="[view_demo/**, build/**]" \
+  --branch-pattern="^master$" \
+  --repo-name=${REPO_NAME} --repo-owner=${REPO_OWNER}
